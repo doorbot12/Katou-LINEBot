3bbd8d777411d71e9af4c23239ac6f47e26dbfb8
doorbot12 <barubeno@gmail.com>
2018-09-05 16:41:31 +0000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  channelAccessToken: "",
-  channelSecret: ""
+  channelAccessToken: "6syESS0Jtsz9BkhWFPVKji0am6yAsKVW3UrkEYofViWfUaI4dN8mJM22vWPdLbtaIn10/FBzg9Z/X2e5xF2/i7vvXXrzPNYxCxk7b5ZfsUiw6ZjlVzlp2VsEG0jMM/jjS7KYoBSC5YtxWKkdwOAm3wdB04t89/1O/w1cDnyilFU=",
+  channelSecret: "110fe4e373688b8806130522706904c5"
 };
 
 export default config;
