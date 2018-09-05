18eaaf725730cafe4b784c0c8a1832a049f0c062
doorbot12 <barubeno@gmail.com>
2018-09-05 23:35:34 +0700

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,32 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var constAPI = {
+  "WIKIPEDIA_URL": "https://id.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=",
+  "OPENWEATHERMAP_URL": "http://api.openweathermap.org/data/2.5/weather?q=",
+  "OPENWEATHERMAP_QUERY": "&units=metric&APPID=",
+  "OPENWEATHERMAP_APPID": "3a16b55d4ac1eb639d68cacf41e42f73",
+  "GOOGLECLOUDAPI_KEY": "AIzaSyDDY6UNml1Qu0Ds8xzo0MZ42yA75DrzUwE",
+  "INSTAGRAM_URL": "https://www.instagram.com/",
+  "YANDEXTRANSLATE_URL": "https://translate.yandex.net/api/v1.5/tr/translate?key=",
+  "YANDEXTRANSLATE_KEY": "trnsl.1.1.20180903T132903Z.556c8891e6a378f3.5748dd4e238bc54a14414f8c156d0e5ea8e9a22c",
+  "YANDEXTEXT_QUERY": "&text=",
+  "YANDEXLANG_QUERY": "&lang=",
+  "YANDEX_OTHERQUERY": "&format=plain&option=",
+  "GMAPSJS_URL": "https://maps.googleapis.com/maps/api/geocode/json?address=",
+  "GMAPSJS_QUERY": "&key=",
+  "GMAPSJS_KEY": "AIzaSyCQSq8CIgQH51RHN47aCD_AMMGDaNqTKCk",
+  "CHARTAPI_URL": "https://chart.apis.google.com/chart?chs=300x50&cht=p3&chtt=",
+  "CHARTAPI_QUERY": "&chts=FFFFFF,24&chf=bg,s,000000",
+  "$9GAG_URL": "https://9gag.com/",
+  "MP3YOUTUBE_URL": "http://mp3you.tube/get/?direct=",
+  "MASHAPE_LOVEMETERURL": "https://love-calculator.p.mashape.com/getPercentage?fname=",
+  "MASHAPE_LOVEMETERQUERY": "&sname=",
+  "MASHAPE_APPKEY": "",
+  "OSUAPI_KEY": "AIzaSyA9B1sPGnF0i5HTomv5d59-JMqNYfokkTg",
+  "KATOULEAVEIMG_URL": "https://image.ibb.co/kYhBYa/1d1827a76c94bfacc205047717824948279041b1_hq.jpg"
+};
+
+exports.default = constAPI;
